2f47bc53825d327b21a479595b89d0a082c68d3d
Andre Roussakoff <madrus@gmail.com>
2014-10-31 20:41:14 +0100

    initial check in

---
+++ BootstrapSandbox/BootstrapSandbox/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BootstrapSandbox.MvcApplication" Language="C#" %>
---
+++ BootstrapSandbox/BootstrapSandbox/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BootstrapSandbox.Startup))]
+namespace BootstrapSandbox
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
