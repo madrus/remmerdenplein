0688a0f9c07601353b30c5f46ccd04503d0f7db4
Andre Roussakoff <madrus@gmail.com>
2015-01-19 21:14:38 +0100

    module.controller statements combined with functions

--- MessageBoard/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MessageBoard.MvcApplication" Language="C#" %>
--- MessageBoard/Services/IMailService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MessageBoard.Services
-{
-    public interface IMailService
-    {
-        bool SendMail(string from, string to, string subject, string body);
-    }
-}
